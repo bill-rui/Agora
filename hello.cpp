59a8e4b250e25413c6b7ad20f8ed18dbf9366da0
Bill Rui <qr2@rice.edu>
2021-06-07 16:56:42 -0500

    new hello file

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int addition(int a, int b){
+    return a + b;
+}
+
+int main() {
+    std::cout << "Hello World!"<<std::endl;
+    int a = 100;
+    std::string s = "hi";
+    std::cout << addition(a, a) << std::endl;
+}
+
