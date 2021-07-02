003894006fbc50cf77e02d82cd0e7069987ff3fc
bill-rui <74115078+bill-rui@users.noreply.github.com>
2021-07-02 18:45:36 -0500

    .

--- test/jenkins_test/build_ue.sh
+++ test/jenkins_test/build_ue.sh
@@ -1,3 +1,5 @@
+source /opt/intel/compilers_and_libraries_2020.3.279/linux/bin/compilervars.sh intel64
+
 echo "==========================================="
 echo "Build cmake... with -DUSE_ARGOS=on -DUSE_UHD=off"
 echo "==========================================="
