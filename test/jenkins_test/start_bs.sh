361f22011cbdc996b0bac3447ff0c5c870f5780e
Bill Rui <qr2@rice.edu>
2021-07-10 19:26:41 -0500

    .

--- test/jenkins_test/start_bs.sh
+++ test/jenkins_test/start_bs.sh
@@ -2,6 +2,7 @@
 # exit code 20 if start fails, 21 if timed out after radio start, 22 if can't find bs out file
 
 source /opt/intel/compilers_and_libraries_2020.3.279/linux/bin/compilervars.sh intel64 > /dev/null
+out_file='test/jenkins_test/bs_out.txt'
 
 for i in 1 2 3; do
 {  # try to start radio at most three times
