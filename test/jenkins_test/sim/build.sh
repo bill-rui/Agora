5ef0d03d8aac895a3ce762adea36e62f42a11be0
Bill Rui <qr2@rice.edu>
2021-07-12 15:15:01 -0500

    .

--- test/jenkins_test/sim/build.sh
+++ test/jenkins_test/sim/build.sh
@@ -1,20 +1,23 @@
 #!/bin/bash -el
+# add flag -UE to generate data file
 source /opt/intel/compilers_and_libraries_2020.3.279/linux/bin/compilervars.sh intel64 > /dev/null
 
 echo "==========================================="
-echo "Run cmake... with -DUSE_ARGOS=on -DUSE_UHD=off (UE)"
+echo "Run cmake... ($1)"
 echo "==========================================="
-cmake -S ./ -B ./build -DUSE_ARGOS=on -DUSE_UHD=off
+cmake -S ./ -B ./build
 echo -e "-------------------------------------------------------\n\n\n"
 
 echo "==========================================="
-echo "execute make (UE)"
+echo "execute make... ($1)"
 echo "==========================================="
 make -j -C ./build
 echo -e "-------------------------------------------------------\n\n\n"
 
-echo "==========================================="
-echo "Generating data for test (UE)"
-echo "==========================================="
-./build/data_generator --conf_file data/ue-ul-hw.json
-echo -e "-------------------------------------------------------\n\n\n"
+if [ "$1" == '-UE' ]; then
+    echo "==========================================="
+    echo "Generating data for test... ($1)"
+    echo "==========================================="
+    ./build/data_generator --conf_file data/tddconfig-sim-ul.json
+    echo -e "-------------------------------------------------------\n\n\n"
+fi
