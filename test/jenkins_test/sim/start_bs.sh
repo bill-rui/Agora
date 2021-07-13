296b64e91a4c36a8ad8c83a7393c28d28baa12f4
Bill Rui <qr2@rice.edu>
2021-07-13 13:17:30 -0500

    .

--- test/jenkins_test/sim/start_bs.sh
+++ test/jenkins_test/sim/start_bs.sh
@@ -13,5 +13,16 @@ fi
 echo "==========================================="
 echo "starting receiver..."
 echo "==========================================="
-./build/agora --conf_file data/tddconfig-sim-ul.json &
+./build/agora --conf_file data/tddconfig-sim-ul.json > $out_file &
 pid=$!
+
+sleep 2
+timeout 120 tail -f -n0 $out_file | grep -qe "Agora: terminating"
+if [ $? != 0 ]; then
+    echo "[Warning] Agora timed out, exiting..."
+    exit 20
+    else
+        echo "test completed!"
+        kill $pid
+        exit
+fi
