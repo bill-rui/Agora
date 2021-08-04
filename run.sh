98fcbee897caf34d87406d052c66f422db4970c5
bill-rui <74115078+bill-rui@users.noreply.github.com>
2021-08-04 17:15:35 -0500

    base station side streaming may or may not be working...

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 sleep 1
 ./build/chsim --bs_threads 1 --ue_threads 1 --worker_threads 2 --core_offset 28 --bs_conf_file data/bs-mac-dl-sim.json --ue_conf_file data/ue-mac-dl-sim.json &
 sleep 1
-./build/macuser --enable_slow_start 1 --conf_file data/ue-mac-dl-sim.json &
+./build/macuser --conf_file data/ue-mac-dl-sim.json &
 
-./build/agora --conf_file data/bs-mac-sim.json &
-./build/macbs --enable_slow_start 1 --conf_file data/bs-mac-sim.json
+./build/agora --enable_slow_start 1 --conf_file data/bs-mac-dl-sim.json &
+./build/macbs --enable_slow_start 1 --conf_file data/bs-mac-dl-sim.json
